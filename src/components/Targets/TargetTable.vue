36914cb250da7f6381851d5744d6fa22d4870b87
David Collom <dcollom@lco.global>
2021-06-10 15:15:55 -0700

    Added axios calls to get dynamic target data

--- src/components/Targets/TargetTable.vue
+++ src/components/Targets/TargetTable.vue
@@ -1,18 +1,28 @@
 <template>
     <b-container>
         <div>This is a TargetTable</div>
-        <b-table striped :items="items">
+        <b-table 
+            striped
+            :fields="targetFields"
+            :items="targets">
         </b-table>
     </b-container>
 </template>
 <script>
+import axios from 'axios';
 
 export default {
     name: 'TargetTable',
     components: {},
     data() {
         return {
-            items: [
+            targetFields: [
+                { 'key': 'id', 'sortable': true },
+                { 'key': 'name', 'sortable': true },
+                { 'key': 'ra' },
+                { 'key': 'dec' },
+            ],
+            targets: [
                 {'name': 'M31', 'type': 'SIDEREAL', 'observations': 2, 'saved_data': 2},
                 {'name': 'M41', 'type': 'SIDEREAL', 'observations': 0, 'saved_data': 0},
             ]
@@ -22,6 +32,15 @@ export default {
         initializeDataEndpoint: function() {
             return 'http://localhost:8000/api/targets/'; // TODO: make this configurable
         },
+    },
+    mounted() {
+        axios
+            .get('http://localhost:8000/api/targets/')
+            .then(response => {
+                console.log(response);
+                this.targets = response['data']['results'];
+                console.log(this.targets);
+            });
     }
 }
 </script>
