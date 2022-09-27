32de1fb41877a1592bd047ed37167b0bde4463b6
Jon <jnation@lco.global>
2022-09-27 22:13:49 +0000

    Move target table and list from tom-demo to this component lib

--- src/components/Targets/TargetList.vue
+++ src/components/Targets/TargetList.vue
@@ -1,17 +1,54 @@
 <template>
-    <b-container>
-        <div>This is a TargetList</div>
-        <target-table></target-table>
-    </b-container>
+  <div id="target-list">
+    <ttk-target-table :selectable="selectable" :targets="targets">
+    </ttk-target-table>
+  </div>
 </template>
+
 <script>
-import TargetTable from '@/components/Targets/TargetTable.vue';
+import { getDataMixin } from '@/mixins';
 
 export default {
-    name: 'TargetList',
-    components: {
-        TargetTable,
-        // TargetFilters
-    }
-}
+  name: 'TargetList',
+  components: {},
+  mixins: [getDataMixin],
+  data() {
+    return {
+      targets: [],
+    };
+  },
+  props: {
+    tomApiBaseUrl: {
+      type: String,
+      required: true
+    },
+    selectable: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+  },
+  methods: {
+    // these methods override stubs in the getDataMixin
+    initializeDataEndpoint: function () {
+      return this.tomApiBaseUrl + '/api/targets/';
+    },
+    onSuccessfulRetrieval: function (response) {
+      // extract the target data from the reponse
+      this.targets = response['data']['results'];
+      return response; // like overridden method in getDataMixin
+    },
+  }
+};
 </script>
+
+<style>
+#target-list {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+};
+</style>
